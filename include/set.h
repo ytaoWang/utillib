289c5b687e09712615fa65896c6a7bea7f43f09b
yt0907.Wang@gmail.com <yt0907.Wang@gmail.com>
2012-08-03 17:42:11 +0800

    implementation set

--- include/set.h
+++ include/set.h
@@ -2,14 +2,16 @@
 #define __SET_H_
 
 struct set {
-    unsigned int rank;
+    unsigned int rank; //calculate rand bottom up
     struct set *parent;
-    struct set *child;
-    struct set *next;
+    struct set *pre_child;
+    struct set *next_child;
+    struct set *pre_sibling;
+    struct set *next_sibling;
 };
 
 struct set *set_new(void);
-int set_union(struct set *,struct set *);
+void set_union(struct set *,struct set *);
 struct set *set_find(struct set *);
 void set_free(struct set *);
 
--- lib/set.c
+++ lib/set.c
@@ -3,14 +3,62 @@
 
 #include "set.h"
 
+static void __remove_self(struct set *s)
+{
+    struct set *prev,*next;
+    
+    prev = s->pre_sibling;
+    next = s->next_sibling;
+    
+    if(prev)
+        prev->next_sibling = next;
+    if(next)
+        next->pre_sibling = prev;
+}
+
+static void __add_child(struct set *parent,struct set *child)
+{
+    struct set *prev,*next;
+    
+    __remove_self(child);
+    
+    child->parent = parent;
+    prev = parent->pre_child;
+    next = parent->next_child;
+    
+    if(prev) 
+        prev->next_sibling = child;
+    child->pre_sibling = prev;
+    
+    if(next)
+        next->pre_sibling = child;
+    child->next_sibling = next;
+}
+
+
+static void set_link(struct set *ps1,struct set *ps2)
+{
+    
+    if(ps1->rank > ps2->rank) {
+        __add_child(ps1,ps2);
+    } else {
+        __add_child(ps2,ps1);
+        if(ps1->rank == ps2->rank)
+            ps2->rank ++;
+    }
+}
+
+
 struct set *set_new(void)
 {
     struct set *ps;
     
     ps = calloc(1,sizeof(struct set));
     ps->parent = NULL;
-    ps->child = NULL;
-    ps->next = NULL;
+    ps->pre_child = NULL;
+    ps->next_child = NULL;
+    ps->pre_sibling = NULL;
+    ps->next_sibling = NULL;
     ps->rank = 0;
     
     return ps;
@@ -23,31 +71,21 @@ struct set *set_find(struct set *ps)
     return ps;
 }
 
-int set_union(struct set *ps1,struct set *ps2)
+void set_union(struct set *ps1,struct set *ps2)
 {
-    struct set *p1,*p2;
-    
-    p1 = set_find(ps1);
-    p2 = set_find(ps2);
-    
-    if(p1) {
-        ps2->rank = p1->rank + 1;
-        
-    }
-    
+    set_link(set_find(ps1),set_find(ps2));
 }
 
 void set_free(struct set *ps)
 {
     struct set *p,*q;
     
-    p = ps->child;
+    p = ps->next_child;
     
     while(p) {
         q = p;
         set_free(q);
-        p = p->next;
+        p = p->next_child;
     }
-    
 }
 
