e8fa5ac019df97667a162108a57a9af5e241f492
wyt <yt0907.Wang@gmail.com>
2014-07-07 11:25:49 +0800

    fix nested module parse

--- include/config.h
+++ include/config.h
@@ -2,5 +2,14 @@
 #define __CONFIG_H_
 
 int read_config(const char *file);
+const char * conf_strerror(unsigned int error);
+
+// define state error code
+#define E_CONF_NO_ERROR 0
+#define E_CONF_FILE_END 0
+#define E_CONF_INVALID_STATE   1
+#define E_CONF_ILLEGAL_CHAR 2 // illegal char
+#define E_CONF_FILE_NOT_EXIST 3 // file don't exist
+#define E_CONF_FILE_READ 4  // fail to read config file 
 
 #endif
--- lib/read_config.c
+++ lib/read_config.c
@@ -1,4 +1,5 @@
 /**
+ * make cmd: gcc test_readconfig.c ../lib/read_config.c -I ../include/ -o test_readconfig -g
  * parse config file
  * config style:
    
@@ -84,12 +85,13 @@ typedef struct {
     size_t col;
 	size_t pos; // conf buffer's position
 	size_t len; // conf buffer's length
-	unsigned int symbol; // delimiter
+	int symbol; // delimiter
     enum STATE state; // now state
 	char *buffer; // conf's buffer
     char *msg;  
 	char *token; // current token
 	FILE *file; // config file fd
+	int depth; // match module symbol { or }
 } context_config_t;
 
 typedef int (*config_func_t)(context_config_t *context);
@@ -120,7 +122,8 @@ static int on_symbol_value(context_config_t *context);
 static int on_symbol_comment(context_config_t *context);
 
 static int __skip_buffer(const char *buffer,size_t *pos, unsigned int c);
-
+static void __skip_symbol(const char *buffer,size_t *pos);
+static int __get_next_symbol(const char *buffer);
 
 static context_state_operation state_func[] = {
 	{ STATE_START,NULL,"function:NULL"},
@@ -178,11 +181,11 @@ static int __istoken(const char *token)
 	while(*token != '\0' && isalnum(*token++))
 		;
 
-	return *(token-1) == '\0';
+	return *token == '\0';
 
 }
 
-static void inline __reset(char *token, unsigned int *symbol)
+static void inline __reset(char *token, int *symbol)
 {
 	memset(token,'\0',_CONFIG_TOKEN);
 	*symbol = 0;
@@ -209,10 +212,10 @@ static int _read_config(const char *filename)
 	char buffer[_CONFIG_BUFFER];
 	char token[_CONFIG_TOKEN];
 	size_t pos = 0,len = 0;
-	context_config_t context = {1,0,0,0,0,STATE_START,NULL,NULL,NULL,NULL}; // start state
+	context_config_t context = {1,0,0,0,0,STATE_START,NULL,NULL,NULL,NULL,0}; // start state
 
 	memset(buffer,'\0',_CONFIG_BUFFER);
-	memset(buffer,'\0',_CONFIG_TOKEN);
+	memset(token,'\0',_CONFIG_TOKEN);
 
 	context.buffer = buffer;
 	context.token = token;
@@ -234,18 +237,38 @@ static int _read_config(const char *filename)
 	context.len = len;
 	context.file = file;
 
-	_parse_state(&context);
+	ret = _parse_state(&context);
+
+	if(ret != E_CONF_NO_ERROR)
+		fdebug_error("fail to parse file:%s\n",conf_strerror(ret));
+
+ _invalid:
+	if(context.buffer[context.pos] == '\0')
+		goto _last;
+
+	__skip_symbol(context.buffer + context.pos,&context.pos);
+
+	if(! __iscomment(context.buffer[context.pos])) {
+		ret = E_CONF_INVALID_STATE;
+		goto _last;
+	}
+	
+	on_symbol_comment(&context);
+
+	goto _invalid;
+
+ _last:	
 
 	fclose(file);
-	
-	return 0;
+	return ret;
 }
 
 static int _parse_state(context_config_t *context)
 {
 	const char *token = context->token;
-	unsigned int *symbol = &context->symbol;
+	int *symbol = &context->symbol;
 	int ret;
+	
 	// get token
 	// get symbol
 	// parse state and router
@@ -263,49 +286,60 @@ static int _parse_state(context_config_t *context)
 	 *  k --> token
 	 *  v --> token
 	 */
+	fdebug_test("next is to context->state:%s\n",context->token);
+
 	switch(context->state) {
 	case STATE_START:
-		if(!token || ! __istoken(context->token))
-			return E_CONF_ILLEGAL_CHAR;
-		goto __on_module_start;
-	_module_start:
 	case MODULE_START: {  // state S
 		if(!token || ! __istoken(context->token))
 			return E_CONF_ILLEGAL_CHAR;
 		
 		// it's import to differ which state route to MOUDLE_START or SYMBOL_KEY
-		// check code in here get next char to check next state
-
-		__on_module_start:
+		// check code in here get next char to check next state and check by symbol
+		ret = __get_next_symbol(context->buffer + context->pos);
+		if(ret == '\0')
+			return E_CONF_INVALID_STATE;
+		
+		if(__issplit(ret)) {
+			set_context_state(context,SYMBOL_KEY);// next state
+		}
 		if(*symbol == 0 || *symbol != _SYMBOL_LEFT_CHAR)
 			return E_CONF_ILLEGAL_CHAR;
   		on_module_start(context);
+		// before goto _module_end,get token first
+		ret = get_token(context);
+		if(ret != E_CONF_NO_ERROR)
+			return ret;
 		goto _module_end;
-
-		__on_symbol_key_start:
-		goto _symbol_key;
 	}
 		break;
 	_module_end:
 	case MODULE_END:
-		if(token || *symbol != _SYMBOL_RIGHT_CHAR)
+		fdebug_test("IN STATE MODULE_END,token:%s,symbol:%c,context->symbol:%c\n",token,*symbol,context->symbol);
+		if(*token || *symbol != _SYMBOL_RIGHT_CHAR)
 			return E_CONF_ILLEGAL_CHAR;
 		on_module_end(context);
+		return E_CONF_NO_ERROR;
 		break;
 	case BLOCK_START:
 		break;
 	case BLOCK_END:
 		break;
 	case SYMBOL_KEY:
+		set_context_state(context,SYMBOL_VALUE);
+		on_symbol_key(context);
 		break;
 	case SYMBOL_VALUE:
+		on_symbol_value(context);
 		break;
     case SYMBOL_COMMENT:
 		break;
 	case STATE_END:
 		break;
 	}
-	return 0;
+
+	return E_CONF_INVALID_STATE;
+
 }
 
 static int get_token(context_config_t *context)
@@ -317,9 +351,11 @@ static int get_token(context_config_t *context)
 
 	const char *p = buffer + *pos;
 	
+	__reset(context->token,&context->symbol);
+	
 	if(*pos >= context->len) {
 			
-		if((context->len = fread(context->buffer,sizeof(char),_CONFIG_BUFFER,context-<file)) < sizeof(char) && ferror(file))  {
+		if((context->len = fread(context->buffer,sizeof(char),_CONFIG_BUFFER,context->file)) < sizeof(char) && ferror(context->file))  {
 			return E_CONF_FILE_READ;
 		}
 
@@ -332,7 +368,7 @@ static int get_token(context_config_t *context)
 
 	while(*p != '\0') {
 
-		fdebug_debug("current char:%c,pos:%d,buffer:%c.\n",*p,*pos,buffer[*pos]);
+		fdebug_debug("current char(p):%c,pos:%u,size:%u,buffer:%c,token:%s\n",*p,*pos,(p - buffer),buffer[*pos],context->token);
 
 		/*
 		 * check state routing by special character
@@ -342,46 +378,66 @@ static int get_token(context_config_t *context)
 			if(state == SYMBOL_VALUE) 
 				return E_CONF_ILLEGAL_CHAR;
 
-			context->line++;		
+			context->line++;
+			(*pos)++;
+			fdebug_test("isnewline token:%s,pos:%u,char:%s\n",token,*pos,context->buffer + *pos);
+			return 0;
+
 		} else if(__iscomment(*p)) {
 			// ignore automatically,don't use automation
 			on_symbol_comment(context);
 			p = buffer + *pos; // state has reset
+
+			// in state SYMBOL_KEY,comment seen as a delimiter
+			if(state == SYMBOL_VALUE)
+				return 0;
+
 			continue;
 		} else if(__issymbol(*p)) { // special symbol name { or }
 			*token = '\0';
 			context->symbol = *p;
+			(*pos)++;
+			fdebug_test("in symbol token:%s,pos:%u,char:%s\n",token,*pos,context->buffer+*pos);
+			// skip '\t\n' after { or }
+		    __skip_symbol(p+1,pos);
 			return 0;
 		} else if(__isspace(*p)) {  // ignore space
 			p++;
+			(*pos)++;
 			continue; // check next char
 		} else if(__issplit(*p)) {  // get a var
 			*token = '\0';
 			context->symbol = *p;
+			(*pos) ++;
 			return 0;
 		}
 
+		fdebug_test("copy token in here,pos:%u,size:%u,token:%s,p:%s\n",*pos,(p - context->buffer),context->token,p);
 		// comment character
 		*token++ = *p++;
 		context->col++;
 		(*pos)++;
 	}
+
+	return E_CONF_INVALID_STATE;
 }
 
 static int on_module_start(context_config_t *context)
 {
 	size_t *pos = &context->pos;
 	char *token = context->token;
-	unsigned int *symbol = &context->symbol;
+	int *symbol = &context->symbol;
 
 	fdebug_test("on_module_start.\n");
 	// check character then manage it.
 	
 	fdebug_test("on module start pos:%d,row:%d,col:%d.token:%s,current:%c.\n",*pos,context->line,context->col,context->token,*(context->buffer+*pos));
-	fdebug_test("on_module_start module name:%s\n",context->token);
-	
+	fdebug_test("on_module_start module name:%s,symbol:%c\n",context->token,*symbol);
+	getchar();
+
 	__reset(token,symbol);
 
+	context->depth ++;
 	_parse_state(context);
 
 	return 0;
@@ -389,8 +445,30 @@ static int on_module_start(context_config_t *context)
 
 static int on_module_end(context_config_t *context)
 {
-	fdebug_test("on module end.\n");
-	return -1;
+	int symbol;
+	
+	//* state router has to give parse_state
+	symbol = __get_next_symbol(context->buffer + context->pos);
+
+	fdebug_test("on module end:%c,next symbol:%c,pos:%d,current char:%c\n",context->symbol,symbol,context->pos,context->buffer[context->pos]);
+	fdebug_test("current buffer:%s\n",context->buffer + context->pos);
+	getchar();
+	
+	__reset(context->token,&context->symbol);
+
+	if(symbol == '\0')
+		return E_CONF_INVALID_STATE;
+
+	context->depth--;
+	if(context->depth < 0 ) 
+		return E_CONF_INVALID_STATE;
+
+	if(__issplit(symbol)) {
+		set_context_state(context,SYMBOL_KEY);
+		_parse_state(context);
+	}
+	
+	return E_CONF_NO_ERROR;
 }
 
 static int on_block_start(context_config_t *context)
@@ -407,19 +485,40 @@ static int on_block_end(context_config_t *context)
 
 static int on_symbol_key(context_config_t *context)
 {
-	// check previous state
+	char *token = context->token;
+	int *symbol = &context->symbol;
+
 	// get key for this module
 	fdebug_test("on_symbol_key:%s.\n",context->token);
-	// goto next state now
-	set_context_state(context,SYMBOL_VALUE);
+	getchar();
+	__reset(token,symbol);
+	
+	_parse_state(context);
+
 	return 0;
 }
 
 static int on_symbol_value(context_config_t *context)
 {
+	char *token = context->token;
+	int *symbol = &context->symbol;
+
 	// check previous state
 	// get value for last key
-	printf("on_symbol_value:%s.\n",context->token);
+	fdebug_test("on_symbol_value:%s.\n",context->token);
+	getchar();
+	__reset(token,symbol);
+
+	//* state router has to give parse_state
+	*symbol = __get_next_symbol(context->buffer + context->pos);
+
+	if(*symbol == '\0')
+		return E_CONF_INVALID_STATE;
+	if(__issplit(*symbol)) {
+		set_context_state(context,SYMBOL_KEY);
+		_parse_state(context);
+	}
+
 	return -1;
 }
 
@@ -461,4 +560,33 @@ static int __skip_buffer(const char *buffer,size_t *pos, unsigned int c)
 	}
 
 	return -1;
+}
+
+static int __get_next_symbol(const char *buffer) 
+{
+	const char *p = buffer;
+	size_t pos = 0;
+	while(p) {
+		if(__iscomment(*p)) {
+			pos = p - buffer;
+			if(__skip_buffer(buffer,&pos,_NEWLINE_CHAR) < 0) // can't find the _NEWLINE_CHAR
+				return '\0';
+			p = buffer + pos;
+		}
+
+		if(__issplit(*p) || __issymbol(*p)) return *p;
+
+		p++;
+	}
+
+	return '\0';
+}
+
+// skip newline or space after module symbol
+static void __skip_symbol(const char *p,size_t *pos)
+{
+	while(__isnewline(*p) || __isspace(*p)) {
+		p++;
+		(*pos)++;
+	}
 }
